23b9301e86e76387af46144095ef1e6f18a346e1
Pedro Tapadas <aluno18781@ipt.pt>
2018-07-12 18:48:49 +0100

    Criação dos models

---
+++ GOTHouses/Models/Characters.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GOTHouses.Models
+{
+    public class Characters
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Family { get; set; }
+
+        public string Photo { get; set; }
+
+        public string Video { get; set; }
+
+    }
+}
---
+++ GOTHouses/Models/Houses.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GOTHouses.Models
+{
+    public class Houses
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Symbol { get; set; }
+        
+    }
+}
