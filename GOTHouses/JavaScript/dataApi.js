1a9fbe49e696532991864297dd82b91be7c0e864
Pedro Tapadas <aluno18781@ipt.pt>
2018-07-13 03:44:35 +0100

    Criação de uma pasta que contém dois ficheiros de js, um para conter as funçoes que acedem aos dados e outro para conter a logica da aplicação

---
+++ GOTHouses/JavaScript/dataApi.js
@@ -0,0 +1,33 @@
+﻿//gets  the valor getHouses
+function getHouses() {
+
+    var houses = "http://localhost:26012/api/houses";
+
+    return fetch(houses, { headers: { Accept: 'application/json' } })
+        .then(function (resposta) {
+            //debugger;
+            if (resposta.status === 200) {
+
+                return resposta.json();
+
+            } else {
+                return Promise.reject(new Error("Erro ao obter categorias"));
+            }
+        });
+}
+
+
+function getCharacters(charId) {
+
+    var character = "http://localhost:26012/api/houses/" + charId;
+
+    return fetch(character, { headers: { Accept: 'application/json' } })
+        .then(function (resposta) {
+            //debugger;
+            if (resposta.status === 200) {
+                return resposta.json();
+            } else {
+                return Promise.reject(new Error("Erro ao obter categorias"));
+            }
+        });
+}
